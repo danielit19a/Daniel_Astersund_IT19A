244ac87c8412278c67fe2f78af905aba61745baa
danielit19a <70194272+danielit19a@users.noreply.github.com>
2020-09-08 09:39:41 +0200

    räkna medelvärdet

---
+++ Variabler_IO/index.js
@@ -0,0 +1,25 @@
+// skapa en variabel name och tilldela det ditt namn 
+
+//skriv ut Hej namn"
+console.log ("Hej")
+let name = "Daniel"
+let age = 18 
+console.log (`Hej ${name}`)
+
+ // skapa variablerna tal1, tal2, tal3
+ // tilldela dessa variabler med värderna 3,4,5
+ // räkna ut medelvärdet av dem
+ let tal1 = 3
+ let tal2 = 4
+ let tal3 = 5
+
+medelvarde = (tal1+tal2+tal3)/3
+
+// Gör följande utskrift
+// "Medelvärdet av talen 3,4,5 är 4"
+console.log (`medelvärdet av talen ${tal1}+${tal2}+${tal3})/3 är = 4`) 
+
+let langd = prompt("ange värdet för rektangelns längd ")
+
+
+
