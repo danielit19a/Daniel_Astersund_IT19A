825b68cf39ebee7b1882af82fb39ca53a2b067f1
danielit19a <70194272+danielit19a@users.noreply.github.com>
2020-09-17 09:52:30 +0200

    if satser lektion 2

---
+++ programering 1/if satser övning/if_sats2.js
@@ -0,0 +1,15 @@
+console.log ("hej")
+
+let age = prompt ("Hur gamma är du")
+if (age <= 15) {
+    console.log("Får inte kolla på filmen")
+}else  {
+    console.log ("Du får kolla på filmen")
+}   
+
+let åker = prompt ("Hur ofta åker du spårvagn på en måndad?")
+if (åker >= 26) {
+    console.log ("Köp måndadskort som kostar 775kr istället för enkel biljett som kostar 30kr")
+}else {
+    console.log ("Köp enkel biljett istället som kostar 30kr")
+} 
