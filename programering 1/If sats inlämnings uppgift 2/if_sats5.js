53c15a08e2f403dbbbc9aed9a17c5bc2da7c2ccc
danielit19a <70194272+danielit19a@users.noreply.github.com>
2020-09-22 09:18:22 +0200

    Inlämning 2 if satser

---
+++ programering 1/If sats inlämnings uppgift 2/if_sats5.js
@@ -0,0 +1,25 @@
+let vikt = prompt ("Hur mycket väger din handbaggage kg")
+let storlek = prompt ("längd på din handbaggage i cm")
+let bredd = prompt ("bredden på din handbaggage i cm")
+let höjd = prompt ("Höjden på din handbagge i")
+
+if (vikt <= 8) {
+    console.log ("Din vikt är godkänd på handbaggaget")
+}else {
+    console.log ("Din handbaggage är inte godkänd")
+}
+if (storlek <= 55) {
+    console.log ("Din längd är godkänt")
+} else {
+    console.log ("Din längd är inte godkänt")
+}
+if (bredd <= 40) {
+    console.log ("Din bredd är godkänt")
+}else {
+    console.log ("Din bredd är inte godkänt")
+}
+if (höjd <= 23) {
+    console.log ("Din höjd är godkänt")
+}else {
+    console.log ("Din höjd är inte godkänt")
+}
