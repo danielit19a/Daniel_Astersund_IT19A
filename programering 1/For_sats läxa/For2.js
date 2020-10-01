99d3cae5d2bc24886505b3b61716f002bb2417f2
danielit19a <70194272+danielit19a@users.noreply.github.com>
2020-10-01 10:02:08 +0200

    Läxa For

--- programering 1/For_sats läxa/For2.js
+++ programering 1/For_sats läxa/For2.js
@@ -1,7 +1,22 @@
 console.log ("hej")
-let P_jämnatal1 = document.querySelector("#jämnatal1")
+let P_jemnatal1 = document.querySelector("#jemnatal1")
 let p_uddatal1 = document.querySelector("#uddatal1")
-
-for (let i=1; i>100; i++){
-    P_jämnatal1.innerHTML += `${i}`
+let p_lektioner = document.querySelector("#lektioner")
+for (let i=1; i<=100; i++){
+    
+    P_jemnatal1.innerHTML += `${i} `
+    i++
 }
+for (let y=2; y<=100; y++){
+    p_uddatal1.innerHTML += `${y} `
+    y++
+} 
+let name = ["Matematik", "Svenska", "Engelska", "Idrott", "Programering"]
+p_lektioner.innerHTML = "Lektioner"
+ 
+
+for (let k = 0; k< name.length; k++)  {
+    p_lektioner.innerHTML += `<br> ${k+1}. ${name[k]} `
+} 
+
+
