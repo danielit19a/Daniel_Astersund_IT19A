9127acc514280058f8c809b85e15221dcce9404e
danielit19a <70194272+danielit19a@users.noreply.github.com>
2020-10-13 09:39:00 +0200

    funktioner testar

---
+++ programering 1/funktioner/funktioner8.js
@@ -0,0 +1,37 @@
+let name = "Daniel"
+function sayhello(){
+    let age = 18
+    console.log(`Hej ${name}`)
+}
+
+// anropar funktionen sayhello
+sayhello()
+sayhello()
+
+function addera(tal1, tal2){
+let summa = tal1+tal2
+return summa 
+}
+
+console.log(`2+5=${addera(2,5)}`)
+
+function subtrahera(tal1, tal2){
+    let summa = tal1-tal2
+    return summa 
+}
+console.log(`2-5=${subtrahera(2,5)}`)
+
+function dividera(tal1, tal2){
+    if (tal2 == 0){
+        return "Får ej dela med 0"
+    }
+    let summa = tal1/tal2 
+    return summa
+}
+console.log(`10/5=${dividera(10,5)}`)
+
+function multiplikation(tal1, tal2){
+    let summa = tal1*tal2
+    return summa
+}
+console.log(`10*5=${multiplikation(10,5)}`)
