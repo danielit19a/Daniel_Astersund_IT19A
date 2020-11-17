3e1cbd539b634e1aa48a828c73631c549b8c5c46
danielit19a <70194272+danielit19a@users.noreply.github.com>
2020-11-17 09:35:16 +0100

    Strängar övning

--- programering 1/Strings/strings.js
+++ programering 1/Strings/strings.js
@@ -27,7 +27,18 @@ console.log(` Bokstav på index 4 är: ${bokstav5} `)
 // längden av en sträng
 let antal_bokstaver = alfabet.length
 console.log(`Antal bokstäver i alfabetet är ${antal_bokstaver}`)
+/*
+r
 
+Räkna antalet ord i den här meningen: Jag läser IT-programmet på NTI kronhus i Göteborg.
+
+*/
+
+let mening = "Jag läser IT-programet på NTI kronhus i Göteborg"
+let ord = mening.split(" ")
+console.log(ord)
+let antal_ord = ord.length
+console.log(`Antal ord i meningen är ${antal_ord}`)
 // slice 
 
 // sökning 
