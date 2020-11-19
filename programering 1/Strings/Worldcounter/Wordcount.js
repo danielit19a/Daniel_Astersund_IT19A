86a8bd8e3084f2ade9d337706492d0af44e3a397
danielit19a <70194272+danielit19a@users.noreply.github.com>
2020-11-19 09:54:59 +0100

    Word count app

---
+++ programering 1/Strings/Worldcounter/Wordcount.js
@@ -0,0 +1,14 @@
+let textArea = document.querySelector("#textArea")
+let countWordBtn = document.querySelector("#countWordBtn")
+let p_displayWords = document.querySelector("#displayWords")
+
+countWordBtn.addEventListener("click", countWords)
+
+function countWords(){
+    console.log("kör funktionen countWords")
+    let text = textArea.value 
+    console.log(text)
+    let words = text.split(" ")
+    console.log(words.length)
+    p_displayWords.innerHTML = words.length
+}
