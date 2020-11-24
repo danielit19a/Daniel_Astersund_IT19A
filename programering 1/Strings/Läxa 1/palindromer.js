8178589ec272ab15f746ce29f44acb44e223735c
danielit19a <70194272+danielit19a@users.noreply.github.com>
2020-11-24 09:35:27 +0100

    Palindromer Övning

---
+++ programering 1/Strings/Läxa 1/palindromer.js
@@ -0,0 +1,8 @@
+let Text = "Hej"
+console.log(Text)
+let letters = Text.split("")
+console.log(letters)
+let backlänges = letters.reverse();
+console.log(backlänges)  
+let Länka = backlänges.join("");
+console.log(Länka)
