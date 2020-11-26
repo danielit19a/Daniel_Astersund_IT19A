4740571e0faf4d2b0c269d92f1c417dc851a51ea
danielit19a <70194272+danielit19a@users.noreply.github.com>
2020-11-26 10:09:56 +0100

    Läxa palindrom

--- programering 1/Strings/Läxa 1/palindromer.js
+++ programering 1/Strings/Läxa 1/palindromer.js
@@ -1,8 +1,30 @@
-let Text = "Hej"
-console.log(Text)
-let letters = Text.split("")
-console.log(letters)
-let backlänges = letters.reverse();
-console.log(backlänges)  
-let Länka = backlänges.join("");
-console.log(Länka)
+let TextArea = document.querySelector("#Textarea") 
+let Txt1 = document.querySelector("#Text1")
+let Txt2 = document.querySelector("#Text2")
+let BTN = document.querySelector("#BTN")
+let Txt3 = document.querySelector("#Text3")
+
+BTN.addEventListener("click",Palindrom)
+function Palindrom(){
+    let Text = TextArea.value
+    console.log(Text)
+    let letters = Text.split("")
+    console.log(letters)
+    let backlänges = letters.reverse();
+    console.log(backlänges)  
+    let Länka = backlänges.join("");
+    console.log(Länka)  
+    if (Text == Länka) {
+        console.log("Det är en Palindrom")
+            } else {
+                console.log("Det är inte en Palindrom")
+                Txt1.innerHTML += Text
+                Txt2.innerHTML += Länka
+               
+            }
+      
+     
+        
+}
+
+
